9323d20f35d9feaa651a3f3c3a2de5206647ae44
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-09-19 13:29:09 +0330

    first commit

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+  return (
+    <div className="w-full text-white bg-[#1F1F21]">
+        <div className="w-10/12 mx-auto py-4">
+
+      Todo list 
+        </div>
+    </div>
+  );
+}
